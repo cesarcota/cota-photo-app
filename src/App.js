cfac2e32e29d33f4d0bbfbe60dbdc91aadaf9692
MonicaGarcia15 <monica.ffgarcia@gmail.com>
2020-03-18 23:11:33 -0100

    cota first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Gallery from "react-photo-gallery"
+import { photos } from  "./photos"
+import NavBar, { ElementsWrapper } from 'react-scrolling-nav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        const navbarItems = [{
+            label: "Item 1",
+            target: "item-1"
+        }, {
+            label: "Item 2",
+            target: "item-2"
+        }, {
+            label: "Item 3",
+            target: "item-3"
+        }, {
+            label: "Item 4",
+            target: "item-4"
+        }, {
+            label: "Item 5",
+            target: "item-5"
+        }, {
+            label: "Item 6",
+            target: "item-6"
+        }, ]
+        return (
+                        <Gallery photos={photos} direction={"column"} />
+        );
+    }
 }
 
 export default App;
